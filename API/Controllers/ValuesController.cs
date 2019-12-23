1d15860f0b6a577d188c94887ed0ae410b045d68
erayizgin <erayizgin@gmail.com>
2019-12-23 19:40:31 +0300

    create ef core and context files apply migration for persistence and api projects

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"value1", "value2"};
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+        // POST api/values
+        // [HttpPost]
+        // public void Post([FromBody] string value)
+        // {
+        //     return "value";
+        // }
+    }
+}
